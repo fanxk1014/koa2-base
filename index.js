e492de5402a2f6c00075fcd73e994516a63ab386
xiaokun <fanxk1014@foxmail.com>
2019-04-16 11:50:12 +0800

    更改配置

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Koa = require('koa')
 const app = new Koa()
-
+ 
 app.use(async(ctx)=>{
-    ctx.body = 'hello xiaokun'
+  ctx.body = 'hello koa2'
 })
-
+ 
 app.listen(3000)
-console.log('[demo] is starting at port 3000')
+console.log('[demo] start-quick is starting at port 3000')
---
+++ test.js
@@ -0,0 +1,3 @@
+async function testAsync(){
+    return 'Hello world'
+}
